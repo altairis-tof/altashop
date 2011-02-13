01f51c2f43aed6a1238b330535ab20e2e01e4a0b
Christophe Battarel <christophe@altairis.fr>
2011-02-13 17:48:03 +0100

    Ajout répertoires parameters et temporary

---
+++ parameters/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * the place were YACS manages and uses configuration data
+ *
+ * @author Bernard Paques
+ * @reference
+ * @license http://www.gnu.org/copyleft/lesser.txt GNU Lesser General Public License
+ */
+if(!isset($context) || !is_array($context))
+	echo 'Shared librairies - move away';
+?>
